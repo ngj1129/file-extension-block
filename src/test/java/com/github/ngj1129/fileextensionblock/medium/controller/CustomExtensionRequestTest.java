a9123f020d786a997dfa641a747cdc4e5f62c2bd
ngj1129 <grw13141@naver.com>
2026-01-04 15:23:34 +0900

    feat: 커스텀 확장자 추가 api 서비스 테스트, 컨트롤러 단 request 테스트 추가

---
+++ src/test/java/com/github/ngj1129/fileextensionblock/medium/controller/CustomExtensionRequestTest.java
@@ -0,0 +1,58 @@
+package com.github.ngj1129.fileextensionblock.medium.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.github.ngj1129.fileextensionblock.extensionblock.service.ExtensionBlockService;
+import com.github.ngj1129.fileextensionblock.extensionblock.web.ExtensionBlockController;
+
+@WebMvcTest(controllers = ExtensionBlockController.class)
+class CustomExtensionRequestTest {
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@MockitoBean
+	ExtensionBlockService extensionBlockService;
+
+	@Test
+	@DisplayName("확장자 빈 값이면 400")
+	void ext_blank_returns400() throws Exception {
+		mockMvc.perform(post("/api/extensions/custom")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("""
+					{"ext": "   "}
+				"""))
+			.andExpect(status().isBadRequest());
+	}
+
+	@Test
+	@DisplayName("확장자 null이면 400")
+	void ext_null_returns400() throws Exception {
+		mockMvc.perform(post("/api/extensions/custom")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("""
+					{"ext": null}
+				"""))
+			.andExpect(status().isBadRequest());
+	}
+
+	@Test
+	@DisplayName("확장자 20자 초과면 400")
+	void ext_over20_returns400() throws Exception {
+		mockMvc.perform(post("/api/extensions/custom")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("""
+					{"ext": "abcdefghijklmnopqrstu"}
+				"""))
+			.andExpect(status().isBadRequest());
+	}
+}
---
+++ src/test/java/com/github/ngj1129/fileextensionblock/medium/service/ExtensionBlockServiceTest.java
@@ -0,0 +1,98 @@
+package com.github.ngj1129.fileextensionblock.medium.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.github.ngj1129.fileextensionblock.extensionblock.common.exception.CustomExtensionAlreadyExistsException;
+import com.github.ngj1129.fileextensionblock.extensionblock.common.exception.CustomExtensionLimitExceededException;
+import com.github.ngj1129.fileextensionblock.extensionblock.common.exception.FixedExtensionNotFoundException;
+import com.github.ngj1129.fileextensionblock.extensionblock.domain.CustomExtension;
+import com.github.ngj1129.fileextensionblock.extensionblock.repository.CustomExtensionRepository;
+import com.github.ngj1129.fileextensionblock.extensionblock.repository.FixedExtensionRepository;
+import com.github.ngj1129.fileextensionblock.extensionblock.service.ExtensionBlockService;
+import com.github.ngj1129.fileextensionblock.extensionblock.web.dto.request.CustomExtensionRequest;
+import com.github.ngj1129.fileextensionblock.extensionblock.web.dto.request.FixedExtensionUpdateRequest;
+
+@ExtendWith(MockitoExtension.class)
+class ExtensionBlockServiceTest {
+
+	@InjectMocks
+	ExtensionBlockService extensionBlockService;
+
+	@Mock
+	FixedExtensionRepository fixedExtensionRepository;
+
+	@Mock
+	CustomExtensionRepository customExtensionRepository;
+
+	@Test
+	@DisplayName("경로의 ext에 해당하는 고정 확장자가 존재하지 않으면 예외를 반환한다")
+	void updateFixedExtension_throwIfNotFound() {
+		// given
+		String ext = "exe";
+		given(fixedExtensionRepository.findByExt(ext))
+			.willReturn(Optional.empty());
+
+		// when & then
+		assertThatThrownBy(() -> extensionBlockService.updateFixedExtension(
+			ext, new FixedExtensionUpdateRequest(true)))
+			.isInstanceOf(FixedExtensionNotFoundException.class)
+			.hasMessageContaining("존재하지 않는 고정 확장자");
+
+		then(fixedExtensionRepository).should(times(1)).findByExt(ext);
+	}
+
+	@Test
+	@DisplayName("커스텀 확장자가 이미 200개 있으면 예외를 반환한다")
+	void createCustomExtension_throwIfLimitExceeded() {
+		// given
+		given(customExtensionRepository.count()).willReturn(200L);
+
+		// when & then
+		assertThatThrownBy(() -> extensionBlockService.createCustomExtension(new CustomExtensionRequest("exe")))
+			.isInstanceOf(CustomExtensionLimitExceededException.class);
+	}
+
+	@Test
+	@DisplayName("커스텀 확장자가 중복이면 예외를 반환한다")
+	void createCustomExtension_throwIfDuplicated() {
+		// given
+		given(customExtensionRepository.count()).willReturn(0L);
+		given(customExtensionRepository.existsByExt("exe")).willReturn(true);
+
+		// when & then
+		assertThatThrownBy(() -> extensionBlockService.createCustomExtension(new CustomExtensionRequest("exe")))
+			.isInstanceOf(CustomExtensionAlreadyExistsException.class);
+	}
+
+	@Test
+	@DisplayName("커스텀 확장자를 만들면 id를 반환한다")
+	void createCustomExtension_returnsId() {
+		// given
+		given(customExtensionRepository.count()).willReturn(0L);
+		given(customExtensionRepository.existsByExt("exe")).willReturn(false);
+
+		CustomExtension saved = CustomExtension.builder()
+			.id(1L)
+			.ext("exe")
+			.build();
+
+		given(customExtensionRepository.save(any(CustomExtension.class)))
+			.willReturn(saved);
+
+		// when
+		Long id = extensionBlockService.createCustomExtension(new CustomExtensionRequest("exe"));
+
+		// then
+		assertThat(id).isEqualTo(1L);
+	}
+}
